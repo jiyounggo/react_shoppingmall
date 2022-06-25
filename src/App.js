b4510414edbf39b0d8643e6cf4c8284c44d3b7c1
leegiyoung <jssa2468@naver.com>
2022-06-25 16:32:09 +0900

    Add : 더보기 버튼 추가하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbars from "./components/header.js";
 import Main from "./pages/main.js";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbars />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,77 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import "../style/App.css";
 import Slider from "../components/slider.js";
+import Data from "../data/data.js";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { Alert } from "react-bootstrap";
 
 function Main() {
+  let [item, setitem] = useState(Data);
+  let [버튼, 버튼변경] = useState(0);
   return (
     <div>
       <Slider />
+      <div className="item">
+        {item.map((a, i) => {
+          return (
+            <div className="items" key={i}>
+              <Link to={`/detail/${a.id}`}>
+                <Card item={item[i]} i={i}></Card>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+
+      <button
+        className="addBt"
+        onClick={() => {
+          버튼변경(버튼 + 1);
+        }}
+      >
+        더보기
+      </button>
+      <Clicks 버튼={버튼} item={item} setitem={setitem}></Clicks>
+    </div>
+  );
+}
+function Clicks(props) {
+  useEffect(() => {
+    if (props.버튼 == 1) {
+      axios
+        .get("https://codingapple1.github.io/shop/data2.json")
+        .then((result) => {
+          let copy = [...props.item, ...result.data];
+          props.setitem(copy);
+        })
+        .catch((err) => console.log(err));
+    }
+    if (props.버튼 == 2) {
+      axios
+        .get("https://codingapple1.github.io/shop/data3.json")
+        .then((result) => {
+          let copy = [...props.item, ...result.data];
+          props.setitem(copy);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [props.버튼]);
+
+  if (props.버튼 == 3) {
+    return alert("상품이 더 없습니다");
+  }
+}
+function Card(props) {
+  return (
+    <div>
+      <img
+        className="itemIMG"
+        src={process.env.PUBLIC_URL + "img/clothes" + props.i + ".png"}
+      ></img>
+      <h4>{props.item.title}</h4>
+      <p>{props.item.content}</p>
     </div>
   );
 }
