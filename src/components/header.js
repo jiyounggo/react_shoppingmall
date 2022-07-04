9b1b39c15364e7df329c0583c92ec42a730e422d
leegiyoung <jssa2468@naver.com>
2022-07-04 19:43:17 +0900

    Add :로그아웃 기능 구현

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Find from "../components/find.js";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import firebase from "../firebase.js";
 
 import {
   Navbar,
@@ -16,9 +17,11 @@ import "bootstrap/dist/css/bootstrap.css";
 
 function Header() {
   const navigate = useNavigate();
-
-  let [search, setsearch] = useState([]);
-
+  const user = useSelector((state) => state.userSlice);
+  const LogoutHandler = () => {
+    firebase.auth().signOut();
+    navigate("/");
+  };
   return (
     <>
       <Navbar bg="light" expand="lg">
@@ -26,12 +29,22 @@ function Header() {
           <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Nav.Link>Home</Nav.Link>
+            <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+              <Link to="/"> Home</Link>
+              {user.accessToken == "" ? (
+                <Link
+                  to="/login"
+                  style={{
+                    color: "black",
+                    textDecoration: "none",
+                    marginLeft: "30px",
+                  }}
+                >
+                  login
+                </Link>
+              ) : (
+                <div onClick={() => LogoutHandler()}>Logout</div>
+              )}
               <NavDropdown title="Link" id="navbarScrollingDropdown">
                 <NavDropdown.Item>Action</NavDropdown.Item>
                 <NavDropdown.Item>Another action</NavDropdown.Item>
